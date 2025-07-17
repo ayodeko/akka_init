29c1e52229b10046fa729c9726dcd3c619532810
Ayooluwa Akindeko <akindekoayooluwa@gmail.com>
2025-07-17 13:44:06 +0200

    Add expected event Messages

---
+++ Akka_NET_Init/CounterModule/Messages.cs
@@ -0,0 +1,20 @@
+﻿using Akka.Actor;
+
+namespace Akka_NET_Init.CounterModule;
+
+public static class DocumentCommands
+{
+    public sealed record ProcessDocument(string RawText);
+}
+
+public static class CounterCommands
+{
+    public sealed record CountTokens(IReadOnlyList<string> Tokens);
+
+    public sealed record ExpectNoMoreTokens();
+}
+
+public static class CounterQueries
+{
+    public sealed record FetchCounts(IActorRef Subscriber);
+}
